4463452fea4ec41b578928022c9d6d7f483e54f4
wudping <dongpingx.wu@intel.com>
2017-03-27 11:00:05 +0800

    dd: just for test pointer no free xxxx fff01
    Signed-off-by: wudping <dongpingx.wu@intel.com>

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
 
 int main(int argc, char* argv[])
 {
+    char a[]= "dddd";
+    char *dd=new char[10];
+    printf("hello worldxxxxxxxxxxxxxxxx ffff!\n");
     printf("hello world!\n");
     return 0;
 }
